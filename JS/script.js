a801b0c8db7b4277465498c883bddcef776659e4
distruct <qqeyu925@gmail.com>
2020-02-25 12:44:18 +0100

    add JS/upd about

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+var drink;
+function() {
+	// body...
+}
+
+var dialog = document.querySelector('dialog');
+   document.querySelector('#openDialog').onclick = function() {
+    dialog.show(); // Показываем диалоговое окно
+   }
+   document.querySelector('#closeDialog').onclick = function() {
+    dialog.close(); // Прячем диалоговое окно
+   }
