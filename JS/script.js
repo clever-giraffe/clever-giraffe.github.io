fc75706bb139273cae8f2abc761f7900f675e469
distruct <qqeyu925@gmail.com>
2020-05-04 21:32:14 +0300

    fin

--- JS/script.js
+++ JS/script.js
@@ -2,11 +2,3 @@ var drink;
 function() {
 	// body...
 }
-
-var dialog = document.querySelector('dialog');
-   document.querySelector('#openDialog').onclick = function() {
-    dialog.show(); // Показываем диалоговое окно
-   }
-   document.querySelector('#closeDialog').onclick = function() {
-    dialog.close(); // Прячем диалоговое окно
-   }
